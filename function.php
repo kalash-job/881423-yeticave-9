ab21bc701afa50626ba793cf39ffab34e35ba80d
Nikolay Kalashnikov <kalash-job@yandex.ru>
2019-04-18 07:11:46 +0300

    Шаблонизация проекта + Возврат округления в функцию редактирования цены

--- function.php
+++ function.php
@@ -1,13 +1,36 @@
 <?php
 declare(strict_types=1);
 
-function format_cost(int $cost): string
+function format_cost($cost): string
 {
     $result = "";
-    if ($cost >= 1000) {
+    if (ceil($cost) >= 1000) {
         $result = number_format($cost, 0, "", " ");
     }   else {
         $result = $cost;
     }
     return $result . " <b class=\"rub\">р</b>";
+}
+
+/**
+ * Подключает шаблон, передает туда данные и возвращает итоговый HTML контент
+ * @param string $name Путь к файлу шаблона относительно папки templates
+ * @param array $data Ассоциативный массив с данными для шаблона
+ * @return string Итоговый HTML
+ */
+function include_template($name, array $data = []) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
 }
