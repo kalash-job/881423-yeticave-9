62bd2e8fba5483fa4adca648aaffa57c665c0873
Nikolay Kalashnikov <kalash-job@yandex.ru>
2019-04-26 22:59:51 +0300

    Пишем SQL-запросы - корректировка запроса с 3-мя таблицами

--- queries.sql
+++ queries.sql
@@ -18,10 +18,10 @@ INSERT INTO lot
 (name, description, url, price, completion_date, bid_step, category_id, user_id)
 VALUES ("2014 Rossignol District Snowboard",
         "The Rossignol District Amptek Snowboard is an user-friendly freestyle board for the aspiring park and pipe riders.",
-        "img/lot-1.jpg", 10999, "2019-04-26 05:48:05", 300, 1, 1),
+        "img/lot-1.jpg", 10999, "2019-04-29 05:48:05", 300, 1, 1),
        ("DC Ply Mens 2016/2017 Snowboard",
         "Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив снег мощным щелчком и четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот снаряд отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом кэмбер позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется, просто посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла равнодушным.",
-        "img/lot-2.jpg", 159999, "2019-04-26 05:48:05", 450, 1, 2),
+        "img/lot-2.jpg", 159999, "2019-04-29 05:48:05", 450, 1, 2),
        ("Крепления Union Contact Pro 2015 года размер L/XL",
         "Легкая и прочная база Duraflex™ CP3 MD из запатентованного нейлонового композита, пяточная дуга из прочного экструдированного алюминия Extruded 3D (она не изменит своей формы вне зависимости от нагрузки на нее), амортизационные подушки Vaporlite и Multidensity Thermoformed EVA Bushing (они отвечают за отзывчивость и гашение вибраций), а также удобный хайбэк СР3, рассчитанный на самые разные стили катания и удобные стрепы Classic Lite и Ultragrip.",
         "img/lot-3.jpg", 8000, "2019-04-28 05:48:05", 200,
@@ -32,11 +32,11 @@ VALUES ("2014 Rossignol District Snowboard",
         2),
        ("Куртка для сноуборда DC Mutiny Charocal",
         "Наружная часть из мягкого гладкого материала. Водонепроницаемость до 10,000 мм, дышащая отделка до 5000 г/м",
-        "img/lot-5.jpg", 7500, "2019-04-26 09:48:05", 250, 4,
+        "img/lot-5.jpg", 7500, "2019-04-29 09:48:05", 250, 4,
         1),
        ("Маска Oakley Canopy",
         "Увеличенный объем линзы и низкий профиль оправы маски Canopy способствуют широкому углу обзора, а специальное противотуманное покрытие поможет ориентироваться в условиях плохой видимости. Технология вентиляции O-Flow Arch и прослойка из микрофлиса сделают покорение горных склонов более комфортным.",
-        "img/lot-6.jpg", 5400, "2019-05-25 05:48:05", 100, 6, 2);
+        "img/lot-6.jpg", 5400, "2019-05-28 05:48:05", 100, 6, 2);
 
 INSERT INTO bid
     (date, bid_amount, user_id, lot_id)
@@ -49,19 +49,21 @@ SELECT name
 FROM category;
 
 /*получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории*/
-SELECT lot.name                                                                AS lot_name,
-       price,
-       url,
-       category.name                                                           AS category_name,
-       MAX(GREATEST(COALESCE(price, bid_amount), COALESCE(bid_amount, price))) AS price
-FROM lot
-         LEFT JOIN category
-                   ON lot.category_id = category.id
-         LEFT JOIN bid
-                   ON lot.id = bid.lot_id
-WHERE completion_date > now()
-GROUP BY lot.id
-ORDER BY creation_date DESC
+SELECT l.name,
+       l.price,
+       l.url,
+       c.name   AS category_name,
+       l.id,
+       l.creation_date,
+       MAX(GREATEST(COALESCE(l.price, b.bid_amount), COALESCE(b.bid_amount, l.price))) AS current_price
+FROM lot l
+         LEFT JOIN category c
+                   ON l.category_id = c.id
+         LEFT JOIN bid b
+                   ON l.id = b.lot_id
+WHERE l.completion_date > now()
+GROUP BY l.id, l.name, l.url, l.price, l.creation_date, c.name
+ORDER BY l.creation_date DESC
 LIMIT 6;
 
 
